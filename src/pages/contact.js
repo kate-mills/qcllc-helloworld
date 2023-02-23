cf808fdb59387cb870d7e62f33b87fa127cd809d
Kate Mills <helloworldloveksm@icloud.com>
2023-02-23 11:50:52 -0800

    contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,19 +2,19 @@ import * as React from 'react'
 
 import Layout from '../components/layout'
 import Map from '../components/map'
-import { Stack, Text } from '@chakra-ui/react'
+import { Stack, Text, Box } from '@chakra-ui/react'
 
 export default function Contact() {
   return (
     <Layout>
       <article>
         <Map />
-        <Stack spacing={3}>
-          <Text fontSize="lg">Contact Information</Text>
-          <Text fontSize="md">
-            Clear and easy-to-find contact information, including phone number,
-            email address, and physical address.
-          </Text>
+
+        <Stack spacing={2}>
+    <Box height="20px"/>
+          <Text as="h2">QUALITY CONSTRUCTION LLC</Text>
+          <Text as="address"> 1650 ZANKERS RD STE 230 SAN JOSE, CA 95112 </Text>
+          <Text as="a" href="tel:+14086398516">(408) 639-8516</Text>
         </Stack>
       </article>
     </Layout>
