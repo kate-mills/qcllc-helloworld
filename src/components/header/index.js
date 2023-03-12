c9ad0127dc0b9de5bb2c04699c6e4a54d4705a3c
Kate Mills <helloworldloveksm@icloud.com>
2023-03-11 19:17:13 -0800

    better spacing

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,37 +1,43 @@
 import * as React from 'react'
+
 import { Link } from 'gatsby'
 import Logo from '../Logo'
 
-import {
-  Flex,
-  Box,
-  Spacer,
-  Heading,
-  Button,
-  ButtonGroup,
-} from '@chakra-ui/react'
+import { Flex, WrapItem, Button } from '@chakra-ui/react'
 
-export const Header = ()=> {
+export const Header = () => {
   return (
     <Flex
-      padding={3}
+      p={5}
       as="header"
-      minWidth="max-content"
       alignItems="center"
-      gap="2"
+      justifyContent="space-between"
+      backgroundColor={'var(--chakra-colors-whiteAlpha-500)'}
     >
-      <Box p="2"><Heading size="md" color="#8b8b8b"><Link to="/"><Logo fixedWidth="200px"/></Link>
-    </Heading>
-      </Box>
-      <Spacer />
-      <ButtonGroup gap="2">
-        <Button as={Link} to="/contact" colorScheme={"uigold"} size="xs" variant={'ghost'}>
-          Contact
+      <Button p={3} maxW={224} as={Link} to="/" variant="none">
+        <Logo color={'#8b8b8b'} />
+      </Button>
+      <WrapItem flexWrap="wrap">
+        <Button
+          as={Link}
+          to="/contact"
+          size="xs"
+          colorScheme="blackAlpha"
+          variant="ghost"
+        >
+          {' '}
+          Contact{' '}
         </Button>
-        <Button as={Link} to="/developments" colorScheme={'uigold'} size="xs" variant={'ghost'}>
+        <Button
+          as={Link}
+          to="/developments"
+          size="xs"
+          colorScheme="blackAlpha"
+          variant="ghost"
+        >
           Developments
         </Button>
-      </ButtonGroup>
+      </WrapItem>
     </Flex>
   )
 }
